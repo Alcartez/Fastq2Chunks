2176ea55f762220086144b859d7bd1634852cd8c
Alcartez <67427418+Alcartez@users.noreply.github.com>
2021-03-26 16:40:31 +0530

    Update fastq2chunks.py

--- fastq2chunks.py
+++ fastq2chunks.py
@@ -1,10 +1,13 @@
 import os
 from Bio import SeqIO
+from multiprocessing import Pool
+import time
 
-#Def Bash from python
+start = time.time()
+
+
 def sh(script):
     os.system(script)
-
 
 #Define Batch iterator
 def batch_iterator(iterator, batch_size):
@@ -22,6 +25,15 @@ def batch_iterator(iterator, batch_size):
         if batch:
             yield batch
 
+#Define Main function
+def break_fastq(file_path):
+    record_iter = SeqIO.parse(open(file_path), "fastq")
+    for i, batch in enumerate(batch_iterator(record_iter, 1000000)):
+        filename = "chunk_%i.fastq" % (i + 1)
+        with open(filename, "w") as handle:
+            count = SeqIO.write(batch, handle, "fastq")
+        print("Wrote %i records to %s" % (count, filename))
+
 print("Running Fastq2Chunks....")
 
 # Define directory for sample file
@@ -29,7 +41,7 @@ home_dir = os.getcwd()
 test_dir = home_dir + "/test"
 sample_file_path = test_dir + "/test.fastq"
 test_output_dir = test_dir + "/output"
-sh("mkdir test/output")
+sh("mkdir test/output;")
 
 print("Configuring directories ...")
 
@@ -38,12 +50,11 @@ os.chdir(test_output_dir)
 
 print("Splitting fastq, prepare for a barrage of output...")
 
-#Time to split
-record_iter = SeqIO.parse(open(sample_file_path), "fastq")
-for i, batch in enumerate(batch_iterator(record_iter, 1000000)):
-    filename = "chunk_%i.fastq" % (i + 1)
-    with open(filename, "w") as handle:
-        count = SeqIO.write(batch, handle, "fastq")
-    print("Wrote %i records to %s" % (count, filename))
+#Time to split and pool
+p = Pool()
+p.map(break_fastq(sample_file_path),[])
 
+end = time.time()
+duration = str(end - start)
 print("It is done! Celebrate!")
+print("Runtime : "  + duration)
