4a03fdc3776df2d7195e4712c81454ddd0b3e42a
Alcartez <67427418+Alcartez@users.noreply.github.com>
2021-03-26 10:20:41 +0530

    Update fastq2chunks.py

--- fastq2chunks.py
+++ fastq2chunks.py
@@ -1,15 +1,38 @@
 import os
 from Bio import SeqIO
 
-#BeepBopNoises
+#Define Batch iterator
+def batch_iterator(iterator, batch_size):
+    entry = True 
+    while entry:
+        batch = []
+        while len(batch) < batch_size:
+            try:
+                entry = next(iterator) #for Python2 iterator.next()
+            except StopIteration:
+                entry = None
+            if entry is None:
+                break
+            batch.append(entry)
+        if batch:
+            yield batch
+
+
+# Define directory for sample file
 home_dir = os.getcwd()
-fastq2split_dir = home + "/fastq2split"
+fastq2split_dir = home_dir + "/fastq2split"
 test_dir = fastq2split_dir + "/test"
 sample_file_path = test_dir + "/test.fastq"
+print("Configuring directories ...")
+test_output_dir = test_dir + "/output"
 
+#Define Output Directory
+os.chdir(test_output_dir)
+
+#Time to split
 record_iter = SeqIO.parse(open(sample_file_path), "fastq")
 for i, batch in enumerate(batch_iterator(record_iter, 10000)):
-    filename = "group_%i.fastq" % (i + 1)
+    filename = "chunk_%i.fastq" % (i + 1)
     with open(filename, "w") as handle:
         count = SeqIO.write(batch, handle, "fastq")
     print("Wrote %i records to %s" % (count, filename))
