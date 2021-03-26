f952519cd15d2091355c32491fee9b18a1a73864
Alcartez <67427418+Alcartez@users.noreply.github.com>
2021-03-26 10:59:20 +0530

    Update fastq2chunks.py

--- fastq2chunks.py
+++ fastq2chunks.py
@@ -1,6 +1,11 @@
 import os
 from Bio import SeqIO
 
+#Def Bash from python
+def sh(script):
+    os.system(script)
+
+
 #Define Batch iterator
 def batch_iterator(iterator, batch_size):
     entry = True 
@@ -17,22 +22,28 @@ def batch_iterator(iterator, batch_size):
         if batch:
             yield batch
 
+print("Running Fastq2Chunks....")
 
 # Define directory for sample file
 home_dir = os.getcwd()
 test_dir = home_dir + "/test"
 sample_file_path = test_dir + "/test.fastq"
 test_output_dir = test_dir + "/output"
+sh("mkdir test/output")
 
 print("Configuring directories ...")
 
 #Define Output Directory
 os.chdir(test_output_dir)
 
+print("Splitting fastq, prepare for a barrage of output...")
+
 #Time to split
 record_iter = SeqIO.parse(open(sample_file_path), "fastq")
-for i, batch in enumerate(batch_iterator(record_iter, 10000)):
+for i, batch in enumerate(batch_iterator(record_iter, 1000000)):
     filename = "chunk_%i.fastq" % (i + 1)
     with open(filename, "w") as handle:
         count = SeqIO.write(batch, handle, "fastq")
     print("Wrote %i records to %s" % (count, filename))
+
+print("It is done! Celebrate!")
